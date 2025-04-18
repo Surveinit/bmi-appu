52c3636ae3998d87f7e96f171dfba7939a578348
Surve <surve8729@gmail.com>
2025-04-18 23:47:16 +0530

    Calculate and Display the BMI

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,43 @@ export default function App() {
   const [height, setHeight] = useState("");
   const [weight, setWeight] = useState("");
 
+  const [bmi, setBmi] = useState("");
+  const [category, setCategory] = useState("");
+
+  function calculateBmi() {
+    const h = parseFloat(height);
+    const w = parseFloat(weight);
+
+    if (!h || !w || h <= 0 || w <= 0) {
+      setBmi(null);
+      setCategory("Please enter valid numbers!");
+      return;
+    }
+
+    const heightInMeters = h / 100;
+    const bmiValue = w / (heightInMeters * heightInMeters);
+    const roundedBmi = bmiValue.toFixed(2);
+
+    setBmi(roundedBmi);
+
+    if (bmiValue < 18.5) {
+      setCategory("Underweight");
+    } else if (bmiValue < 24.9) {
+      setCategory("Normal");
+    } else if (bmiValue < 29.9) {
+      setCategory("Overweight");
+    } else {
+      setCategory("Obese");
+    }
+  }
+
+  function resetForm() {
+    setHeight("");
+    setWeight("");
+    setBmi("");
+    setCategory("");
+  }
+
   return (
     <div>
       <h1>BMI appu</h1>
@@ -32,7 +69,17 @@ export default function App() {
         </label>
       </div>
 
-      <button>Calculate 🙂‍↔️</button>
+      <button onClick={calculateBmi}>Calculate 🙂‍↔️</button>
+      <button onClick={resetForm}>Reset 🔄</button>
+
+      {bmi && (
+        <div>
+          <h2>Your Bmi,</h2>
+          <h3>
+            {bmi} ({category})
+          </h3>
+        </div>
+      )}
     </div>
   );
 }
